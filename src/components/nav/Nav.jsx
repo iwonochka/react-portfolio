695f03b3aa2c49d1cdb7f29909b9230c1786314e
Iwona Behnke <behnke.iwona@gmail.com>
2022-07-09 23:24:59 +0200

    Implemented on click change of state

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,14 +5,17 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {MdApps} from 'react-icons/md'
 import {MdOutlineEmail} from 'react-icons/md'
+import {useState} from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#portfolio"><MdApps /></a>
-      <a href="#contact"><MdOutlineEmail /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdApps /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineEmail /></a>
     </nav>
   )
 }
