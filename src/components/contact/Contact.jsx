7ac4c87f0cd58d839c6459b18f02719e79362492
Iwona Behnke <behnke.iwona@gmail.com>
2022-07-11 12:19:20 +0200

    First version of contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './contact.css'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_zy1omve', 'template_ynhwjpf', form.current, 'rb8kKgp1_v11AY1CU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your full name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+
+    </section>
   )
 }
 
