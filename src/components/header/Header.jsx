2a138966d03dad5a5aedc6f48aab9021d2e0273a
Iwona Behnke <behnke.iwona@gmail.com>
2023-03-02 16:10:30 +0100

    Social links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,6 @@ import ME from './../../assets/me.png'
 import AOS from "aos";
 import { HiChevronDoubleDown } from 'react-icons/hi'
 import { useEffect } from "react";
-/*import {BsArrowDown} from 'react-icons/bs'*/
 
 const Header = () => {
 
@@ -20,30 +19,21 @@ const Header = () => {
         <h2>Iwona Behnke</h2>
         <h5 className='text-light'>Junior full-stack web developer</h5>
 
-        {/* <div className='container header-details__container'>
-          <div className='me'>
-            <img src={ME} alt="me" />
-          </div>
-        </div> */}
-
-      <div className='container about__container' data-aos="fade-right" data-aos-duration="1500">
-
-        <div className='container about__me'>
-
-          <div className="about__me-image">
-            <img src={ME} alt="me" />
+        <div className='container about__container' data-aos="fade-right" data-aos-duration="1500">
+          <div className='container about__me'>
+            <div className="about__me-image">
+              <img src={ME} alt="me" />
+            </div>
           </div>
 
+          <div className='container about__content' data-aos="fade-left" data-aos-duration="1500">
+            <div className='about__content-text'>
+              <p>I'm a full-stack developer, graduated from 2 intense full-time web development bootcamps: <a href="https://www.lewagon.com/berlin/web-development-course" target='_blank' rel="noreferrer">Le Wagon</a> (Ruby on Rails) and <a href="https://www.ironhack.com/en/web-development/berlin?utm_source=google&utm_medium=cpc&utm_campaign=BER_Germany_Berlin_Global_Search_Brand_EN&utm_content=search-brand&utm_term=ironhack%20web%20development&gclid=EAIaIQobChMIiuOs6467_QIV7AqiAx1SwgPdEAAYASAAEgILo_D_BwE" target='_blank' rel="noreferrer">Ironhack</a> (JavaScript and React), based in Berlin, Germany.</p>
+              <p>As a former communication trainer I used to conduct trainings for one of the biggest companies in Germany. Now I'm combining my communication skills and passion for tech as a coding teacher!</p>
+            </div>
+            <CTA />
+          </div>
         </div>
-
-        <div className='container about__content' data-aos="fade-left" data-aos-duration="1500">
-          <div className='about__content-text'>
-            <p>I'm a full-stack developer, graduated from 2 intense full-time web development bootcamps: <a href="https://www.lewagon.com/berlin/web-development-course">Le Wagon</a> (Ruby on Rails) and <a href="https://www.ironhack.com/en/web-development/berlin?utm_source=google&utm_medium=cpc&utm_campaign=BER_Germany_Berlin_Global_Search_Brand_EN&utm_content=search-brand&utm_term=ironhack%20web%20development&gclid=EAIaIQobChMIiuOs6467_QIV7AqiAx1SwgPdEAAYASAAEgILo_D_BwE">Ironhack</a> (JavaScript and React), based in Berlin, Germany.</p>
-            <p>As a former communication trainer I used to conduct trainings for one of the biggest companies in Germany. Now I'm combining my communication skills and passion for tech as a coding teacher!</p>
-          </div>
-          <CTA />
-        </div>
-      </div>
 
         <div>
           <a href="#contact" className='scroll__down'><HiChevronDoubleDown /></a>
